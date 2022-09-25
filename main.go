56e1d659fe50472dafee850455c9bcf7e42f7c7b
labile <1a6i1e@gmail.com>
2022-09-26 02:25:17 +0300

    add update interval

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strconv"
+	"time"
 )
 
 type Input struct {
@@ -41,39 +43,68 @@ type LibInput struct {
 	ScrollButton    int     `json:"scroll_button,omitempty"`
 }
 
+type Output struct {
+	Language string `json:"text"`
+}
+
+//goland:noinspection GoSnakeCaseUsage
+const DEFAULT_UPDATE_INTERVAL = 1 * time.Second
+
 func main() {
+	Layout()
+
+}
+
+func getUpdateInterval() time.Duration {
+	i := os.Getenv("SWAY_KEYBOARD_LAYOUT_UPDATE_INTERVAL")
+	if i == "" {
+		return DEFAULT_UPDATE_INTERVAL
+	}
+
+	t, err := strconv.Atoi(i)
+	if err != nil {
+		return DEFAULT_UPDATE_INTERVAL
+	}
+
+	return time.Duration(t) * time.Second
+}
+
+func Layout() {
 	var (
 		out bytes.Buffer
 		err error
 	)
+	for {
+		cmd := exec.Command("swaymsg", "-r", "-t", "get_inputs")
+		cmd.Stdout = &out
 
-	cmd := exec.Command("swaymsg", "-r", "-t", "get_inputs")
-	cmd.Stdout = &out
-
-	if err = cmd.Run(); err != nil {
-		log.Fatal(err)
-	}
-
-	var inputs []Input
-	if err = json.NewDecoder(&out).Decode(&inputs); err != nil {
-		log.Fatal(err)
-	}
-
-	if layout, err := GetFirstActiveKbdLayout(inputs); err != nil {
-		log.Fatal(err)
-	} else {
-		if _, err = os.Stdout.WriteString(layout); err != nil {
+		if err = cmd.Run(); err != nil {
 			log.Fatal(err)
 		}
+
+		var inputs []Input
+		if err = json.NewDecoder(&out).Decode(&inputs); err != nil {
+			log.Fatal(err)
+		}
+
+		if layout, err := getFirstActiveKbdLayout(inputs); err != nil {
+			log.Fatal(err)
+		} else {
+			if err := json.NewEncoder(os.Stdout).Encode(layout); err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		time.Sleep(getUpdateInterval())
 	}
 }
 
-func GetFirstActiveKbdLayout(inputs []Input) (string, error) {
+func getFirstActiveKbdLayout(inputs []Input) (Output, error) {
 	for _, input := range inputs {
 		if input.XkbActiveLayoutName != "" {
-			return input.XkbActiveLayoutName, nil
+			return Output{Language: input.XkbActiveLayoutName}, nil
 		}
 	}
 
-	return "", errors.New("no active keyboard layout found")
+	return Output{}, errors.New("no active keyboard layout found")
 }
